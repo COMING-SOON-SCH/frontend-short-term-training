ad9adfcc071c6c717752b6218d27a733615b32eb
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-09-05 19:59:53 +0900

    ✨ feat : 인풋 폼 데이터 입력받기 기능 개발
    입력된 값을 text 상태를 변경시켜 "TodoItem" 컴포넌트에서 출력되게끔 기능을 개발함.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Input = ({ placeholder }) => {
-  return <input type="text" placeholder={placeholder} className="todo-input" />;
+const Input = ({ placeholder, text, handleChange }) => {
+  return <input type="text" placeholder={placeholder} className="todo-input" value={text} onChange={handleChange} />;
 };
 
 export default Input;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,12 +5,24 @@ import Button from "./Button";
 import "../assets/todo.css";
 
 const Todo = () => {
+  const [text, setText] = useState("");
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+  useEffect(() => {
+    console.log(text); // 얘를 실행하셈
+  }, [text]);
+
+  // 배열 변경 감지
+  useEffect(() => {
+    console.log(arrayList); // 얘를 실행하셈
+  }, [arrayList]);
   return (
     <div className="todo-container">
       <h1>투두리스트</h1>
       <div className="todo-input-container">
-        <Input value="" onChange={() => {}} placeholder="할 일을 입력하세요" />
-        <Button onClick={() => {}}></Button>
+        <Input text={text} handleChange={handleChange} placeholder={"입력하세요"} />
+
       </div>
       <ul className="todo-list">
         <li className="todo-item">
