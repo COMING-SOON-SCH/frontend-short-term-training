16e8f59d095aaf418bb73edca098094f9cb48bbd
Junse0lee <ljs22672744@gmail.com>
2024-09-21 18:14:19 +0900

    ✨feat: 삭제 버튼 생성, 삭제 기능 추가
    삭제 버튼을 만들어 목록에 있는 내용을 삭제

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import Checkbox from "./CheckBox";
+// Todo.js
+import React, { useState } from "react";
 import Input from "./Input";
 import Button from "./Button";
 import "../assets/todo.css";
@@ -7,50 +7,43 @@ import TodoItem from "./TodoItem";
 
 const Todo = () => {
   const [arrayList, setArrayList] = useState([]);
-
-  let RenderTestArr = arrayList.map((el) => {
-    return (
-      <>
-        <TodoItem todoText={el}> </TodoItem>
-      </>
-    );
-  });
-
   const [text, setText] = useState("");
 
   const handleChange = (event) => {
     setText(event.target.value);
   };
 
-  // 페이지가 처음 시작될떄
-  useEffect(() => {
-    console.log(text); // 얘를 실행하셈
-  }, [text]);
-
-  // 배열 변경 감지
-  useEffect(() => {
-    console.log(arrayList); // 얘를 실행하셈
-  }, [arrayList]);
-
-  // 입력 버튼 클릭 시 실행될 함수
-  // 불변성의 원칙
   const handleClick = () => {
-    console.log("버튼 누름");
-    setArrayList([...arrayList, text]);
+    if (text.trim() === "") {
+      alert("내용을 입력하세요!");
+      return;
+    }
+    setArrayList([...arrayList, text]); // 새로운 todo 추가
+    setText("");
   };
+
+  // 할 일 삭제 함수
+  const onDelete = (index) => {
+    setArrayList(arrayList.filter((_, i) => i !== index)); // 인덱스로 삭제
+  };
+
+  const renderTodoItems = arrayList.map((el, index) => (
+    <TodoItem
+      key={index}
+      todoText={el}
+      index={index} // 인덱스를 전달
+      onDelete={onDelete} // 삭제 함수 전달
+    />
+  ));
 
   return (
     <div className="todo-container">
       <h1>투두리스트</h1>
       <div className="todo-input-container">
         <Input text={text} handleChange={handleChange} placeholder={"입력하세요"} />
-
         <Button testFunc={handleClick}>입력</Button>
       </div>
-      <ul className="todo-list">
-        <TodoItem todoText={text}> </TodoItem>
-        {RenderTestArr}
-      </ul>
+      <ul className="todo-list">{renderTodoItems}</ul>
     </div>
   );
 };
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,19 +1,21 @@
+// TodoItem.js
 import React from "react";
 import Button from "./Button";
 import Checkbox from "./CheckBox";
 
-const TodoItem = ({todoText}) => { // 나
+const TodoItem = ({ todoText, index, onDelete }) => {
   return (
-    <div>
-      <li className="todo-item">
-        <Checkbox checked={false} onChange={() => {}} />
-        <span className="todo-text">
-            {todoText}
-        </span>
-        <Button onClick={() => {}}>text</Button>
-        <Button onClick={() => {}}>text</Button>
-      </li>
-    </div>
+    <li className="todo-item">
+      <Checkbox checked={false} onChange={() => {}} />
+      <span className="todo-text">{todoText}</span>
+      <button
+        type="button"
+        className="deleteBtn"           //className을 deleteBtn로 설정
+        onClick={() => onDelete(index)} // 인덱스를 onDelete로 전달
+      >
+        삭제
+      </button>
+    </li>
   );
 };
 
